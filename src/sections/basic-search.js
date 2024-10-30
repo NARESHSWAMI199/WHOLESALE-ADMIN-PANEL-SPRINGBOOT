14928da34dd9419c64ad11cacd1aece36a0b93c9
ashutoshdil <rahul.a@dil.in>
2024-10-30 11:45:34 +0530

    update basic search

--- src/sections/basic-search.js
+++ src/sections/basic-search.js
@@ -51,7 +51,7 @@ export const BasicSearch = (props) => {
       status : 'A',
       type : 'A',
       fromDate : previousDate,
-      toDate : toDate
+      toDate : currentDate
     })
     props.onSearch();
   }
