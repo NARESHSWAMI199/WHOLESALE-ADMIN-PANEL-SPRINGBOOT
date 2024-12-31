5c438e5a46aff0e87ff6bd9d1590ce4bc91c7e40
nareshswami199 <swaminaresh993@gmail.com>
2024-12-31 23:56:43 +0530

    plan is active or not

--- src/main/java/com/sales/wholesaler/controller/WholesaleServicePlanController.java
+++ src/main/java/com/sales/wholesaler/controller/WholesaleServicePlanController.java
@@ -3,16 +3,21 @@ package com.sales.wholesaler.controller;
 
 import com.sales.admin.controllers.ServiceContainer;
 import com.sales.entities.ServicePlan;
+import com.sales.entities.User;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping("wholesale/plans")
+@RequestMapping("wholesale/plan")
 public class WholesaleServicePlanController extends WholesaleServiceContainer {
 
     @GetMapping("/all")
@@ -20,5 +25,14 @@ public class WholesaleServicePlanController extends WholesaleServiceContainer {
         return new ResponseEntity<>(wholesaleServicePlanService.getALlServicePlan(), HttpStatusCode.valueOf(200));
     }
 
+    @GetMapping("is-active")
+    public ResponseEntity<Map<String,Object>> isUserPlanActive(HttpServletRequest request){
+        User user = (User) request.getAttribute("user");
+        Map<String,Object> result = new HashMap<>();
+        boolean planIsActive = wholesaleServicePlanService.isPlanActive(user.getActivePlan());
+        result.put("planIsActive",planIsActive);
+        result.put("status" , 200);
+        return new ResponseEntity<>(result, HttpStatus.valueOf((Integer) result.get("status")));
+    }
 
 }
