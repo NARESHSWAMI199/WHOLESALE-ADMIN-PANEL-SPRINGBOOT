dd4a1bebaff28830ecbb348b612d68f4301de868
ashutoshdil <rahul.a@dil.in>
2025-01-01 16:51:26 +0530

    cors issue resolved

--- src/main/java/com/sales/config/WebSecurityConfig.java
+++ src/main/java/com/sales/config/WebSecurityConfig.java
@@ -13,7 +13,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((auth) -> auth
                 .requestMatchers("/**").permitAll()
-        ).csrf(AbstractHttpConfigurer::disable).cors(AbstractHttpConfigurer::disable);
+        ).csrf(AbstractHttpConfigurer::disable).cors(corsConfigurer -> {});
         return http.build();
     }
 }
