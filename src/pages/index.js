9c82f2b7b10f8c65e431c4b483f9edfb7fc6990b
ashutoshdil <rahul.a@dil.in>
2025-01-10 19:01:55 +0530

    ui update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
-import { Box, Container, Unstable_Grid2 as Grid} from '@mui/material';
+import { Box, Container, Unstable_Grid2 as Grid } from '@mui/material';
 import { Layout as DashboardLayout } from 'src/layouts/dashboard/layout';
-import {OverviewUsers } from 'src/sections/overview/overview-users';
+import { OverviewUsers } from 'src/sections/overview/overview-users';
 import { OverviewLatestUsers } from 'src/sections/overview/overview-latest-users';
 import { OverviewLatestStores } from 'src/sections/overview/overview-latest-stores';
 import { OverviewStores } from 'src/sections/overview/overview-sales';
@@ -19,70 +19,70 @@ import Link from 'next/link';
 
 const now = new Date();
 
-const Page = (props) =>  {
-const [wholesales,setWholesales] = useState([])
-const [users,setAllUsers] = useState([])
-const [currentYearDataMonths, setCurrentYearDataMonths] = useState([])
-const [lastYearDataMonths, setLastYearDataMonths] = useState([])
-  const [years,setYears] = useState([])
-const [dashboardData,setDashboardData] = useState({
-  users : {},
-  retailers : {},
-  wholesalers : {},
-  staffs : {},
-  admins : {},
-})    
-const currentYear = new Date().getFullYear();
-const auth = useAuth()
-const user = auth.user
-const router = useRouter();
+const Page = (props) => {
+  const [wholesales, setWholesales] = useState([])
+  const [users, setAllUsers] = useState([])
+  const [currentYearDataMonths, setCurrentYearDataMonths] = useState([])
+  const [lastYearDataMonths, setLastYearDataMonths] = useState([])
+  const [years, setYears] = useState([])
+  const [dashboardData, setDashboardData] = useState({
+    users: {},
+    retailers: {},
+    wholesalers: {},
+    staffs: {},
+    admins: {},
+  })
+  const currentYear = new Date().getFullYear();
+  const auth = useAuth()
+  const user = auth.user
+  const router = useRouter();
 
 
-// Getting All Stores 
-useEffect( ()=>{
-    const getData = async () => {
-      axios.defaults.headers = {
-        Authorization : auth.token
-      }
-      await axios.post(host+"/admin/store/all",{})
-      .then(res => {
-          const data = res.data.content;
-          setWholesales(data);
-      })
-      .catch(err => {
-        console.log(err)
-      } )
-    }
-    getData();
-
-  },[])
-
-  // Get current year store count
+  // Getting All Stores 
   useEffect(() => {
+    const getData = async () => {
       axios.defaults.headers = {
         Authorization: auth.token
       }
-        axios.post(host + "/admin/dashboard/graph/months/", {
-          "year": currentYear
-        })
+      await axios.post(host + "/admin/store/all", {})
         .then(res => {
-          const months = res.data.res;
-          let counts = []
-          Object.keys(months).map(month => {
-            counts.push(months[month])
-          })
-          setYears(Object.keys(months))
-          setCurrentYearDataMonths(counts);
+          const data = res.data.content;
+          setWholesales(data);
         })
         .catch(err => {
           console.log(err)
-          let status = (!!err.response ? err.response.status : 0);
-          if (status == 401) {
-            auth.signOut();
-            router.push("/auth/login")
-          }
         })
-    }, [])
+    }
+    getData();
+
+  }, [])
+
+  // Get current year store count
+  useEffect(() => {
+    axios.defaults.headers = {
+      Authorization: auth.token
+    }
+    axios.post(host + "/admin/dashboard/graph/months/", {
+      "year": currentYear
+    })
+      .then(res => {
+        const months = res.data.res;
+        let counts = []
+        Object.keys(months).map(month => {
+          counts.push(months[month])
+        })
+        setYears(Object.keys(months))
+        setCurrentYearDataMonths(counts);
+      })
+      .catch(err => {
+        console.log(err)
+        let status = (!!err.response ? err.response.status : 0);
+        if (status == 401) {
+          auth.signOut();
+          router.push("/auth/login")
+        }
+      })
+  }, [])
 
   // Get store count of last year
   useEffect(() => {
@@ -90,7 +90,7 @@ useEffect( ()=>{
       Authorization: auth.token
     }
     axios.post(host + "/admin/dashboard/graph/months/", {
-      "year": currentYear-1
+      "year": currentYear - 1
     })
       .then(res => {
         const months = res.data.res;
@@ -106,55 +106,55 @@ useEffect( ()=>{
   }, [])
 
 
-// Get all basics counts 
-  useEffect( ()=>{
+  // Get all basics counts 
+  useEffect(() => {
     const getData = async () => {
       axios.defaults.headers = {
-        Authorization : auth.token
+        Authorization: auth.token
       }
-      await axios.get(host +"/admin/dashboard/counts")
-      .then(res => {
+      await axios.get(host + "/admin/dashboard/counts")
+        .then(res => {
           const data = res.data;
           setDashboardData(data)
-      })
-      .catch(err => {
-        console.log(err)
-      } )
+        })
+        .catch(err => {
+          console.log(err)
+        })
     }
     getData();
 
-  },[])
+  }, [])
 
 
 
-// All users 
+  // All users 
 
 
-// Getting All Stores 
-useEffect( ()=>{
-  const getData = async () => {
-    axios.defaults.headers = {
-      Authorization : auth.token
+  // Getting All Stores 
+  useEffect(() => {
+    const getData = async () => {
+      axios.defaults.headers = {
+        Authorization: auth.token
+      }
+      await axios.post(host + "/admin/auth/A/all", {})
+        .then(res => {
+          const data = res.data.content;
+          setAllUsers(data);
+        })
+        .catch(err => {
+          console.log(err)
+        })
     }
-    await axios.post(host+"/admin/auth/A/all",{})
-    .then(res => {
-        const data = res.data.content;
-        setAllUsers(data);
-    })
-    .catch(err => {
-      console.log(err)
-    } )
-  }
-  getData();
+    getData();
 
-},[])
+  }, [])
 
 
 
-  const getPercentage = useCallback((currentCount,totalCount) =>{
-    return Math.round((currentCount/totalCount) * 100);
-  },[])
-  return ( <>
+  const getPercentage = useCallback((currentCount, totalCount) => {
+    return Math.round((currentCount / totalCount) * 100);
+  }, [])
+  return (<>
     <Head>
       <title>
         Overview | Swami sales
@@ -175,97 +175,105 @@ useEffect( ()=>{
           <Grid
             xs={12}
             sm={6}
-            lg={!!user  &&  user.id != suId ? 3 : 4}
+            lg={!!user && user.id != suId ? 3 : 4}
           >
-           <Link href={{
-              pathname : "/users"
-            }} style={{textDecoration:'none'}} > <OverviewUsers
-              title="USERS"
-              sx={{ height: '100%' }}
-              value={dashboardData.users}
-            /> </Link>
+            <Link href={{
+              pathname: "/users"
+            }} style={{ textDecoration: 'none' }} > <OverviewUsers
+                title="USERS"
+                sx={{ height: '100%' }}
+                value={dashboardData.users}
+              /> </Link>
           </Grid>
 
 
           <Grid
             xs={12}
             sm={6}
-            lg={!!user &&  user.id != suId ? 3 : 4}
+            lg={!!user && user.id != suId ? 3 : 4}
           >
-          <Link style={{
-            textDecoration:'none'
-           }} 
-            href={{
-              pathname : "/customers"
+            <Link style={{
+              textDecoration: 'none'
             }}
-           > <OverviewUsers
-               title="RETAILERS"
-              sx={{ height: '100%' }}
-              value={dashboardData.retailers}
-            /> </Link>
+              href={{
+                pathname: "/customers"
+              }}
+            > <OverviewUsers
+                title="RETAILERS"
+                sx={{ height: '100%' }}
+                value={dashboardData.retailers}
+              /> </Link>
           </Grid>
 
 
-            <Grid
-              xs={12}
-              sm={6}
-              lg={!!user &&  user.id != suId ? 3 : 4}
-            >
+          <Grid
+            xs={12}
+            sm={6}
+            lg={!!user && user.id != suId ? 3 : 4}
+          >
             <Link style={{
-              textDecoration:'none'
+              textDecoration: 'none'
             }}
-            href={{
-              pathname : "/wholesalers"
-            }}>  <OverviewUsers
+              href={{
+                pathname: "/wholesalers"
+              }}>  <OverviewUsers
                 title="WHOLESALERS"
                 sx={{ height: '100%' }}
                 value={dashboardData.wholesalers}
               /> </Link>
+          </Grid>
+
+
+
+          <Grid
+            xs={12}
+            sm={6}
+            lg={!!user && user.id != suId ? 3 : 4}
+          >
+            <Link style={{
+              textDecoration: 'none'
+            }}
+              href={{
+                pathname: "/wholesalers"
+              }} >  <OverviewUsers
+                title='STAFFS'
+                sx={{ height: '100%' }}
+                value={dashboardData.staffs}
+              /> </Link>
+          </Grid>
+
+
+
+          {(!!user && user.id == suId) ?
+            <Grid
+              xs={12}
+              sm={6}
+              lg={!!user && user.id != suId ? 3 : 4}
+            >
+              <Link style={{
+                textDecoration: 'none'
+              }}
+                href={{
+                  pathname: "/users/SA"
+                }} >  <OverviewUsers
+                  title='ADMINS'
+                  sx={{ height: '100%' }}
+                  value={dashboardData.admins}
+                /> </Link>
             </Grid>
-          
+            :
+            <Grid
+              xs={12}
+              sm={6}
+              lg={!!user && user.id != suId ? 3 : 4}
+            >
 
-
-          <Grid
-            xs={12}
-            sm={6}
-            lg={!!user  &&  user.id != suId ? 3 : 4}
-          >
-          <Link style={{
-            textDecoration:'none'
-           }}
-            href={{
-              pathname : "/wholesalers"
-            }} >  <OverviewUsers
-              title='STAFFS'
-              sx={{ height: '100%' }}
-              value={dashboardData.staffs}
-            /> </Link>
-          </Grid>
-
-
-
-        {!!user && user.id ==  suId &&
-          <Grid
-            xs={12}
-            sm={6}
-            lg={!!user &&  user.id != suId ? 3 : 4}
-          >
-          <Link style={{
-            textDecoration:'none'
-           }} 
-           href={{
-            pathname :"/users/SA"
-          }} >  <OverviewUsers
-              title='ADMINS'
-              sx={{ height: '100%' }}
-              value={dashboardData.admins}
-            /> </Link>
-          </Grid>
+            </Grid>
           }
 
 
 
-   {/*     <Grid
+          {/*     <Grid
             xs={12}
             sm={6}
             lg={3}
@@ -323,9 +331,9 @@ useEffect( ()=>{
           >
             <OverviewTraffic
               chartSeries={[
-                getPercentage(dashboardData.wholesalers.all, dashboardData.users.all), 
-                getPercentage(dashboardData.staffs.all, dashboardData.users.all), 
-                getPercentage(dashboardData.retailers.all, dashboardData.users.all), 
+                getPercentage(dashboardData.wholesalers.all, dashboardData.users.all),
+                getPercentage(dashboardData.staffs.all, dashboardData.users.all),
+                getPercentage(dashboardData.retailers.all, dashboardData.users.all),
               ]}
               labels={['Wholesalers', 'Staffs', 'Retailers']}
               sx={{ height: '100%' }}
@@ -336,9 +344,9 @@ useEffect( ()=>{
             md={6}
             lg={4}
           >
-         <OverviewLatestStores
-              products = {wholesales }
-              // sx={{ height: '100%' }}
+            <OverviewLatestStores
+              products={wholesales}
+            // sx={{ height: '100%' }}
             />
           </Grid>
           <Grid
@@ -348,16 +356,16 @@ useEffect( ()=>{
           >
             <OverviewLatestUsers
               users={users}
-              sx={{ overflowX : 'auto' }}
+              sx={{ overflowX: 'auto' }}
             />
           </Grid>
         </Grid>
       </Container>
     </Box>
   </>
-  
+
   );
- }
+}
 
 Page.getLayout = (page) => (
   <DashboardLayout>
