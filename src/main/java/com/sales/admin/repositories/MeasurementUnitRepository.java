0afb43cb87bd1b4beda9a4b92040ed055a2c8f98
ashutoshdil <rahul.a@dil.in>
2025-02-06 17:21:13 +0530

    optimized imports

--- src/main/java/com/sales/admin/repositories/MeasurementUnitRepository.java
+++ src/main/java/com/sales/admin/repositories/MeasurementUnitRepository.java
@@ -2,7 +2,6 @@ package com.sales.admin.repositories;
 
 import com.sales.entities.MeasurementUnit;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
