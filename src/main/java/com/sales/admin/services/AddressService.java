ee038f282845740ac969d9c41bfac97d1c3a8e6b
ashutoshdil <rahul.a@dil.in>
2025-02-14 19:04:25 +0530

    added logger in admin services

--- src/main/java/com/sales/admin/services/AddressService.java
+++ src/main/java/com/sales/admin/services/AddressService.java
@@ -6,6 +6,8 @@ import com.sales.entities.City;
 import com.sales.entities.State;
 import com.sales.entities.User;
 import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,8 +18,11 @@ import static com.sales.utils.Utils.getCurrentMillis;
 @Service
 public class AddressService extends RepoContainer {
 
+    private static final Logger logger = LoggerFactory.getLogger(AddressService.class);
+
     @Transactional
-    public Address insertAddress(AddressDto addressDto, User loggedUser){
+    public Address insertAddress(AddressDto addressDto, User loggedUser) {
+        logger.info("Entering insertAddress with addressDto: {}, loggedUser: {}", addressDto, loggedUser);
         Address address = new Address();
         address.setSlug(UUID.randomUUID().toString());
 
@@ -32,15 +37,23 @@ public class AddressService extends RepoContainer {
         address.setCreatedBy(loggedUser.getId());
         address.setUpdatedAt(getCurrentMillis());
         address.setUpdatedBy(loggedUser.getId());
-        return  addressRepository.save(address);
+        Address result = addressRepository.save(address);
+        logger.info("Exiting insertAddress with result: {}", result);
+        return result;
     }
 
-    public List<City> getCityList(int stateId){
-        return addressHbRepository.getCityList(stateId);
+    public List<City> getCityList(int stateId) {
+        logger.info("Entering getCityList with stateId: {}", stateId);
+        List<City> result = addressHbRepository.getCityList(stateId);
+        logger.info("Exiting getCityList with result: {}", result);
+        return result;
     }
 
-    public List<State> getStateList(){
-        return addressHbRepository.getStateList();
+    public List<State> getStateList() {
+        logger.info("Entering getStateList");
+        List<State> result = addressHbRepository.getStateList();
+        logger.info("Exiting getStateList with result: {}", result);
+        return result;
     }
 
 }
