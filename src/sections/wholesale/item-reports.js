0cada422b79a5e389c89651823effc516e1482ae
ashutoshdil <rahul.a@dil.in>
2025-03-13 14:33:40 +0530

    removed  unnessary code from item-reports

--- src/sections/wholesale/item-reports.js
+++ src/sections/wholesale/item-reports.js
@@ -1,30 +1,21 @@
 import {
-    Avatar,
-    Box,
-    Card,
-    Stack,
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TablePagination,
-    TableRow,
-    Typography
+  Avatar,
+  Box,
+  Card,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Typography
 } from '@mui/material';
 import { format } from 'date-fns';
 import PropTypes from 'prop-types';
-
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import { useTheme } from '@mui/material/styles';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { useEffect, useState } from 'react';
-import { rowsPerPageOptions, toTitleCase, userImage } from 'src/utils/util';
+import { useState } from 'react';
 import { getInitials } from 'src/utils/get-initials';
+import { rowsPerPageOptions, toTitleCase, userImage } from 'src/utils/util';
 
 export const ItemReports = (props) => {
   const {
@@ -36,59 +27,7 @@ export const ItemReports = (props) => {
     rowsPerPage = 0,
     selected = []
   } = props;
-  const [itemReports,setItemReports] = useState(props.itemReports)
-  const [message,setMessage] = useState("")
-  const [confirm,setConfirm] = useState(false)
-  const [slug,setSlug] = useState(null)
-  const [rowIndex,setRowIndex] = useState(-1)
-  const [status,setStatus] = useState('')
-  const [action,setAction] = useState('')
-  const [isCopied, setIsCopied] = useState(false);
-  const theme = useTheme();
-  const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
-
-  async function copyTextToClipboard(text) {
-    if ('clipboard' in navigator) {
-      return await navigator.clipboard.writeText(text);
-    } else {
-      return document.execCommand('copy', true, text);
-    }
-  }
-
-  const handleCopyClick = (slug) => {
-    // Asynchronously call copyTextToClipboard
-      copyTextToClipboard(slug)
-      .then(() => {
-        // If successful, update the isCopied state value
-        setItems((items).filter(customer => {
-         if(customer.slug == slug){
-          customer.isCopied = true
-          setIsCopied(true);
-         }
-         return customer
-      }))
-        setTimeout(() => {
-          setItems((items).filter(customer => {
-            if(customer.slug == slug){
-             customer.isCopied = false
-             setIsCopied(false);
-            }
-            return customer
-         }))
-        }, 1500);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-
-  
-
-  const handleClose =  () =>{
-      setConfirm(false)
-  }
-
-
+  const [itemReports] = useState(props.itemReports)
   return ( <>
     <Card sx={{overflowX: 'auto'}}>
         <Box sx={{ minWidth: 800}}>
@@ -172,32 +111,6 @@ export const ItemReports = (props) => {
         rowsPerPageOptions={rowsPerPageOptions}
       />
     </Card>
-
-   
-    <Dialog
-        fullScreen={fullScreen}
-        open={confirm}
-        onClose={handleClose}
-        aria-labelledby="responsive-dialog-title"
-      >
-        <DialogTitle id="responsive-dialog-title">
-          {"Are you sure ?"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-           {message}
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button autoFocus onClick={handleClose}>
-            Disagree
-          </Button>
-          <Button onClick={()=>takeAction(action)} autoFocus>
-            Agree
-          </Button>
-        </DialogActions>
-      </Dialog>
-    
     </>
 
 
