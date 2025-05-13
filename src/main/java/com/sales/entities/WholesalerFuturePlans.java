4cb0dd7941cf00023b3c170dd5ada6616badd8fa
NARESHSWAMI199 <swaminaresh993@gmail.com>
2025-05-13 16:39:18 +0530

    future plans table added

---
+++ src/main/java/com/sales/entities/WholesalerFuturePlans.java
@@ -0,0 +1,36 @@
+package com.sales.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "wholesaler_future_plans")
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class WholesalerFuturePlans {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    long id;
+
+    @Column(name = "user_id",nullable = false)
+    Integer userId;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "plan_id",nullable = false)
+    ServicePlan servicePlan;
+
+    @Column(name = "created_at")
+    Long createdAt;
+
+    @Column(name = "updated_at")
+    Long updatedAt;
+
+    @Column(name="status") // status like used or not.
+    String status;
+
+}
